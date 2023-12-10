5a8e4a7f3251fcd0e7b043768f19d67a71f522e3
Halima-Amur <halim9amer@gmail.com>
2023-12-11 02:49:25 +0400

    anything

---
+++ day13/IntegerMiror.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace day13
+{
+	public static class IntegerMiror
+	{
+		public static int Miror(this int Number)
+		{
+			int result = 0, reminder;
+			while(Number != 0)
+			{
+                reminder = Number % 10;
+
+				result = result * 10 + reminder;
+				Number = Number / 10;
+
+            }
+			return result;
+        }
+	}
+}
+
---
+++ day13/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace day13
+{
+    class program
+    {
+        static void Main()
+        {
+            int x = 12345;
+            int y = (IntegerMiror.Miror(x));
+            Console.WriteLine(y);
+
+            List<int> lst = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int> result = Enumerable.Where(lst, x => x % 2 == 0).ToList();
+            foreach (var xx in result)
+            {
+                Console.WriteLine(xx);
+            }
+            Console.WriteLine(result.GetType());
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var lst1 = numbers.Where(i => i % 2 == 0).ToList();
+            numbers.Remove(2);
+            numbers.AddRange(new int[] { 10, 11, 12, 13, 14, 15 });
+            foreach (var item in lst.ToList())
+            {
+                Console.Write($"{item} ,");
+            }
+            numbers.Remove(2);
+
+        }
+
+    }
+}
+
+
